d950fa1ce1d655da637c8ff30b336d68fd73077b
MelaSqui <dani.v.villa30@gmail.com>
2025-05-19 19:35:21 -0400

    version 0002

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+    const nombre = "Cabecera";
+  return (
+    <header className="flex items-center justify-between p-4 bg-gray-800 text-white">
+        <h1 className="text-2xl font-bold">{nombre}</h1>
+      <nav>
+        <ul className="flex space-x-4">
+          <li><a href="#home" className="hover:underline">Home</a></li>
+          <li><a href="#about" className="hover:underline">About</a></li>
+          <li><a href="#contact" className="hover:underline">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
---
+++ src/components/Titulo.tsx
@@ -0,0 +1,9 @@
+function Titulo() {
+  return (
+    <div className="titulo">
+      <h1>Bienvenido a React!</h1>
+      <p>Desde título <code>src/App.tsx</code> and save to reload</p>
+    </div>
+  );
+}
+export default Titulo;
